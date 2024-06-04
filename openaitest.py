bb98cd111ae47195a49a86b86d3cdd5f3c49867e
Sumanth-Level 0 <112182870+sumanth-0@users.noreply.github.com>
2024-06-04 19:37:16 +0530

    Add files via upload

---
+++ openaitest.py
@@ -0,0 +1,5 @@
+from openai import OpenAI
+from config import apikey
+
+
+openai.api_key = apikey
---
+++ tro.py
@@ -0,0 +1,17 @@
+import win32com.client
+import os
+
+speaker=win32com.client.Dispatch("SAPI.spVoice")
+
+#while 1:
+ #   print("kvnf")
+    #s=input()
+   # speaker.Speak(s)
+    
+os.system("open { \Users\admin99\Downloads\bling_bang_bang_born}")
+
+
+    
+
+
+
